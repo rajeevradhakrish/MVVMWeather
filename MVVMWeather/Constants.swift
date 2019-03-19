0699acea46942d619d489f1281896eaa81ba8d33
Rajeev Radhakrishnan <rajeevradhakrish@gmail.com>
2019-03-19 09:34:32 +0200

    Initial Commit

---
+++ MVVMWeather/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  MVVMWeather
+//
+//  Created by Rajeev Radhakrishnan on 19/03/2019.
+//  Copyright © 2019 Rajeev Radhakrishnan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants
+{
+    struct API
+    {
+        struct URL
+        {
+            static var locationAPI = "https://dataservice.accuweather.com/locations/v1/topcities/50?"
+            static var forecastApi = "https://dataservice.accuweather.com/forecasts/v1/hourly/"
+            static var imageUrl = "https://vortex.accuweather.com/adc2010/m/images/icons/80x80/"
+        }
+        
+        static var apiKey = "EEMi3kJVfqFrm4kk8hIXpCiqDOp07ofU"
+    }
+    
+    struct NavigationSegue {
+        
+    }
+    
+    struct cellIdentifier {
+        
+    }
+}
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MVVMWeather' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MVVMWeather
+
+pod 'SCLAlertView'
+pod 'NVActivityIndicatorView'
+pod 'Alamofire'
+pod 'Kingfisher', '~> 5.1'
+
+  target 'MVVMWeatherTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MVVMWeatherUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
